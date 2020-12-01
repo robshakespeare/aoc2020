9a4286c250f729460f3690a07a489b3df5d91707
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2020-12-01 13:45:02 +0000

    Added in example tests for Day 1

--- AoC.Tests/Day1/Day1SolverTests.cs
+++ AoC.Tests/Day1/Day1SolverTests.cs
@@ -9,6 +9,21 @@ namespace AoC.Tests.Day1
         private readonly Day1Solver _sut = new Day1Solver();
 
         [Test]
+        public void Part1Example()
+        {
+            // ACT
+            var part1Result = _sut.SolvePart1(@"1721
+979
+366
+299
+675
+1456");
+
+            // ASSERT
+            part1Result.Should().Be(514579);
+        }
+
+        [Test]
         public void Part1ReTest()
         {
             // ACT
@@ -16,6 +31,21 @@ namespace AoC.Tests.Day1
 
             // ASSERT
             part1Result.Should().Be(910539);
+        }
+
+        [Test]
+        public void Part2Example()
+        {
+            // ACT
+            var part1Result = _sut.SolvePart2(@"1721
+979
+366
+299
+675
+1456");
+
+            // ASSERT
+            part1Result.Should().Be(241861950);
         }
 
         [Test]
