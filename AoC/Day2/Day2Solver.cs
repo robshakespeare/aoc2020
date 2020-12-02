7cc7dd9ec14b84e3c429db45036b805317706577
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2020-12-02 07:44:00 +0000

    Refactored Day2Solver

--- AoC/Day2/Day2Solver.cs
+++ AoC/Day2/Day2Solver.cs
@@ -4,16 +4,12 @@ namespace AoC.Day2
 {
     public class Day2Solver : SolverBase
     {
-        protected override long? SolvePart1Impl(string input)
-        {
-            IPasswordPolicy policy = new PasswordPolicy1();
-            var lines = input.ReadAllLines().Select(PasswordLine.Parse);
-            return policy.GetValidLines(lines).Count();
-        }
+        protected override long? SolvePart1Impl(string input) => Solve(input, new PasswordPolicy1());
 
-        protected override long? SolvePart2Impl(string input)
+        protected override long? SolvePart2Impl(string input) => Solve(input, new PasswordPolicy2());
+
+        private static long? Solve(string input, IPasswordPolicy policy)
         {
-            IPasswordPolicy policy = new PasswordPolicy2();
             var lines = input.ReadAllLines().Select(PasswordLine.Parse);
             return policy.GetValidLines(lines).Count();
         }
