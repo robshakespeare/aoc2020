dc78ecaac9640d3e9152f1614e45da4cf27304ec
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2020-12-06 05:49:29 +0000

    Day 6, p1 done

--- AoC.Tests/Day6/Day6SolverTests.cs
+++ AoC.Tests/Day6/Day6SolverTests.cs
@@ -9,13 +9,37 @@ namespace AoC.Tests.Day6
         private readonly Day6Solver _sut = new();
 
         [Test]
+        public void Part1Example()
+        {
+            // ACT
+            var part1Result = _sut.SolvePart1(@"abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b");
+
+            // ASSERT
+            part1Result.Should().Be(11);
+        }
+
+        [Test]
         public void Part1ReTest()
         {
             // ACT
             var part1Result = _sut.SolvePart1();
 
             // ASSERT
-            part1Result.Should().Be(null);
+            part1Result.Should().Be(6662);
         }
 
         [Test]
--- AoC/Day6/Day6Solver.cs
+++ AoC/Day6/Day6Solver.cs
@@ -1,11 +1,14 @@
+using System.Linq;
+using static System.Environment;
+
 namespace AoC.Day6
 {
     public class Day6Solver : SolverBase
     {
-        protected override long? SolvePart1Impl(string input)
-        {
-            return base.SolvePart1Impl(input);
-        }
+        protected override long? SolvePart1Impl(string input) => input
+            .Split($"{NewLine}{NewLine}")
+            .Select(group => group.Where(c => c >= 'a' && c <= 'z').Distinct())
+            .Aggregate(0, (accumulate, current) => accumulate + current.Count());
 
         protected override long? SolvePart2Impl(string input)
         {
