377412d0389ac71db18a9c3f4a8fed9085151e1f
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2020-12-06 08:44:11 +0000

    Refactor Day 5 to use improved pattern matching

--- AoC/Day5/Day5Solver.cs
+++ AoC/Day5/Day5Solver.cs
@@ -26,18 +26,20 @@ namespace AoC.Day5
         public int Id { get; } = Row * 8 + Column;
 
         public static Seat Parse(string ticketRef) => new(
-            ParseSection(ticketRef[0..7], (0, 127), 'F', 'B'),
-            ParseSection(ticketRef[7..10], (0, 7), 'L', 'R'));
+            ParseSection(ticketRef[0..7], (0, 127)),
+            ParseSection(ticketRef[7..10], (0, 7)));
 
-        private static int ParseSection(string ticketRefSection, (int start, int end) range, char lower, char upper)
+        private static int ParseSection(string ticketRefSection, (int start, int end) range)
         {
             foreach (var chr in ticketRefSection)
             {
                 int mid = range.start + (range.end - range.start) / 2;
-
-                if (chr == lower) range = (range.start, mid);
-                else if (chr == upper) range = (mid + 1, range.end);
-                else throw new InvalidOperationException($"Unexpected char {chr}");
+                range = chr switch
+                {
+                    'F' or 'L' => (range.start, mid),
+                    'B' or 'R' => (mid + 1, range.end),
+                    _ => throw new InvalidOperationException($"Unexpected char {chr}")
+                };
             }
             return range.start;
         }
