d5669b0c444990494b2b10a8a77f7085424d761f
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2020-12-07 07:30:37 +0000

    Day 7, part 1 done

--- AoC.Tests/Day7/Day7SolverTests.cs
+++ AoC.Tests/Day7/Day7SolverTests.cs
@@ -9,13 +9,31 @@ namespace AoC.Tests.Day7
         private readonly Day7Solver _sut = new();
 
         [Test]
+        public void Part1Example()
+        {
+            // ACT
+            var part1Result = _sut.SolvePart1(@"light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.");
+
+            // ASSERT
+            part1Result.Should().Be(4);
+        }
+
+        [Test]
         public void Part1ReTest()
         {
             // ACT
             var part1Result = _sut.SolvePart1();
 
             // ASSERT
-            part1Result.Should().Be(null);
+            part1Result.Should().Be(142);
         }
 
         [Test]
--- AoC/Day7/Day7Solver.cs
+++ AoC/Day7/Day7Solver.cs
@@ -1,15 +1,106 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
 namespace AoC.Day7
 {
     public class Day7Solver : SolverBase
     {
-        protected override long? SolvePart1Impl(string input)
-        {
-            return base.SolvePart1Impl(input);
-        }
+        protected override long? SolvePart1Impl(string input) => BagRules.Parse(input).CountBagColorsCanContain("shiny gold");
 
         protected override long? SolvePart2Impl(string input)
         {
             return base.SolvePart2Impl(input);
+        }
+    }
+
+    public class BagRules
+    {
+        private readonly BagRule[] _bagRules;
+        private readonly Dictionary<string, BagRule> _bagRulesDictionary;
+
+        public BagRules(params BagRule[] bagRules)
+        {
+            _bagRules = bagRules;
+            _bagRulesDictionary = bagRules.ToDictionary(
+                x => x.BagColor,
+                x => x);
+        }
+
+        /// <summary>
+        /// Counts the number of bag colors that can eventually contain at least one specified color of bag.
+        /// </summary>
+        public long CountBagColorsCanContain(string bagColor)
+        {
+            var validContainingBagColors = new HashSet<string>();
+
+            foreach (var bagRule in _bagRules)
+            {
+                if (CanContain(bagRule, bagColor))
+                {
+                    validContainingBagColors.Add(bagRule.BagColor);
+                }
+            }
+
+            return validContainingBagColors.Count;
+        }
+
+        private bool CanContain(BagRule bagRule, string bagColor)
+        {
+            if (bagRule.CanDirectlyContain(bagColor))
+            {
+                return true;
+            }
+
+            // or, if any of the child bags can contain the `bagColor`
+            return bagRule.CanContainColorQuantity
+                .Any(x => CanContain(_bagRulesDictionary[x.containingBagColor], bagColor));
+        }
+
+        public static BagRules Parse(string input) => new(input.ReadLines().Select(BagRule.Parse).ToArray());
+    }
+
+    public class BagRule
+    {
+        public string BagColor { get; }
+
+        public IEnumerable<(string containingBagColor, int quantity)> CanContainColorQuantity { get; }
+
+        private readonly Dictionary<string, int> _canContainColorQuantityDictionary;
+
+        public BagRule(string bagColor, params (string containingBagColor, int quantity)[] canContainColorQuantity)
+        {
+            BagColor = bagColor;
+            CanContainColorQuantity = canContainColorQuantity;
+            _canContainColorQuantityDictionary = canContainColorQuantity.ToDictionary(
+                x => x.containingBagColor,
+                x => x.quantity);
+        }
+
+        public bool CanDirectlyContain(string bagColor) => _canContainColorQuantityDictionary.ContainsKey(bagColor);
+
+        private static readonly Regex ParseBag = new("^(?<bagColor>.+) bags contain (?<contents>.+)", RegexOptions.Compiled);
+
+        private static readonly Regex ParseBagContents = new(@"((?<quantity>\d+) (?<containingBagColor>.+?) bag)+", RegexOptions.Compiled);
+
+        public static BagRule Parse(string line)
+        {
+            var match = ParseBag.Match(line);
+
+            if (!match.Success)
+            {
+                throw new InvalidOperationException("Invalid bag line: " + line);
+            }
+
+            var bagColor = match.Groups["bagColor"].Value;
+            var contents = match.Groups["contents"].Value;
+
+            return new BagRule(
+                bagColor,
+                ParseBagContents.Matches(contents)
+                    .Select(content => (content.Groups["containingBagColor"].Value, int.Parse(content.Groups["quantity"].Value)))
+                    .ToArray());
         }
     }
 }
