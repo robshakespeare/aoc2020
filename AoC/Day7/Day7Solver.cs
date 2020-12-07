ac42479d72943348748e009878a5dd0345016e6d
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2020-12-07 08:02:24 +0000

    Add day 7's name

--- AoC/Day7/Day7Solver.cs
+++ AoC/Day7/Day7Solver.cs
@@ -2,6 +2,8 @@ namespace AoC.Day7
 {
     public class Day7Solver : SolverBase
     {
+        public override string DayName => "Handy Haversacks";
+
         protected override long? SolvePart1Impl(string input) => BagRules.Parse(input).CountBagColorsCanContain("shiny gold");
 
         protected override long? SolvePart2Impl(string input) => BagRules.Parse(input).CountBagsRequiredInside("shiny gold");
