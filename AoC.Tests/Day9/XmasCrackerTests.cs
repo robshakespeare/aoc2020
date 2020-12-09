21463727e00d884d640b7fe80a1c34151c137b9e
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2020-12-09 07:19:38 +0000

    Start Day 9, misread part 1 totally!

---
+++ AoC.Tests/Day9/XmasCrackerTests.cs
@@ -0,0 +1,42 @@
+using AoC.Day9;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AoC.Tests.Day9
+{
+    public class XmasCrackerTests
+    {
+        [Test]
+        public void Part1Example()
+        {
+            const string? input = @"35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576";
+
+            var sut = XmasCracker.Parse(input, 5);
+
+            // ACT
+            var result = sut.GetWeakness();
+
+            // ASSERT
+            result.Should().Be(127);
+        }
+    }
+}
--- AoC/Day9/Day9Solver.cs
+++ AoC/Day9/Day9Solver.cs
@@ -1,8 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace AoC.Day9
 {
     public class Day9Solver : SolverBase
     {
-        public override string DayName => "";
+        public override string DayName => "Encoding Error";
 
         protected override long? SolvePart1Impl(string input)
         {
@@ -13,5 +17,54 @@ namespace AoC.Day9
         {
             return base.SolvePart2Impl(input);
         }
+    }
+
+    /// <summary>
+    /// eXchange-Masking Addition System (XMAS) encryption cracker.
+    /// </summary>
+    public class XmasCracker
+    {
+        private readonly IEnumerable<long> _numbers;
+        private readonly int _preambleSize;
+
+        public XmasCracker(IEnumerable<long> numbers, int preambleSize)
+        {
+            _numbers = numbers;
+            _preambleSize = preambleSize;
+        }
+
+        public long GetWeakness()
+        {
+            var previousNumbers = new HashSet<long>();
+            var previousSums = new HashSet<long>();
+
+            foreach (var (num, index) in _numbers.Select((num, index) => (num, index)))
+            {
+                var isSameAsLast = previousNumbers.Any() && previousNumbers.Last() == num;
+                if (isSameAsLast)
+                {
+                    throw new InvalidOperationException($"Duplicate num {num} detected at index {index}");
+                }
+
+                var isValid = index < _preambleSize || previousSums.Contains(num);
+                if (!isValid)
+                {
+                    return num;
+                }
+
+                // Update previous sums
+                foreach (var previousNumber in previousNumbers)
+                {
+                    previousSums.Add(previousNumber + num);
+                }
+
+                // Update previous numbers
+                previousNumbers.Add(num);
+            }
+
+            throw new InvalidOperationException("Failed to get weakness");
+        }
+
+        public static XmasCracker Parse(string input, int preambleSize) => new(input.ReadLinesAsLongs(), preambleSize);
     }
 }
