0a84331c4b27ef7041bce5f29adfc6ced095406e
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2020-12-15 07:29:42 +0000

    Day 15, parts 1 and 2 done, using brute force.  I wonder what the optimal way is?

--- AoC.Tests/Day15/Day15SolverTests.cs
+++ AoC.Tests/Day15/Day15SolverTests.cs
@@ -8,6 +8,22 @@ namespace AoC.Tests.Day15
     {
         private readonly Day15Solver _sut = new();
 
+        [TestCase("0,3,6", 436)]
+        [TestCase("1,3,2", 1)]
+        [TestCase("2,1,3", 10)]
+        [TestCase("1,2,3", 27)]
+        [TestCase("2,3,1", 78)]
+        [TestCase("3,2,1", 438)]
+        [TestCase("3,1,2", 1836)]
+        public void Part1Examples(string input, int expectedResult)
+        {
+            // ACT
+            var part1Result = _sut.SolvePart1(input);
+
+            // ASSERT
+            part1Result.Should().Be(expectedResult);
+        }
+
         [Test]
         public void Part1ReTest()
         {
@@ -15,17 +31,30 @@ namespace AoC.Tests.Day15
             var part1Result = _sut.SolvePart1();
 
             // ASSERT
-            part1Result.Should().Be(null);
+            part1Result.Should().Be(1665);
+        }
+
+        [TestCase("0,3,6", 175594)]
+        [TestCase("1,3,2", 2578)]
+        [Ignore("Currently takes ~8 seconds per test case, there must be an efficient way to do it!")]
+        public void Part2Examples(string input, int expectedResult)
+        {
+            // ACT
+            var part2Result = _sut.SolvePart2(input);
+
+            // ASSERT
+            part2Result.Should().Be(expectedResult);
         }
 
         [Test]
+        [Ignore("Currently takes ~8 seconds, there must be an efficient way to do it!")]
         public void Part2ReTest()
         {
             // ACT
             var part2Result = _sut.SolvePart2();
 
             // ASSERT
-            part2Result.Should().Be(null);
+            part2Result.Should().Be(16439);
         }
     }
 }
--- AoC/Day15/Day15Solver.cs
+++ AoC/Day15/Day15Solver.cs
@@ -1,17 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MoreLinq;
+
 namespace AoC.Day15
 {
     public class Day15Solver : SolverBase
     {
-        public override string DayName => "";
+        public override string DayName => "Rambunctious Recitation";
 
-        protected override long? SolvePart1Impl(string input)
+        protected override long? SolvePart1Impl(string input) => PlayTillTurn(input, 2020);
+
+        protected override long? SolvePart2Impl(string input) => PlayTillTurn(input, 30000000);
+
+        private static long? PlayTillTurn(string input, int playToTurnNumber)
         {
-            return base.SolvePart1Impl(input);
+            var memory = Memory.SeedWith(input);
+
+            while (memory.NextTurnNumber <= playToTurnNumber)
+            {
+                if (memory.HasLastNumberBeenSpokenBefore(out var turnsItWasSpoken))
+                {
+                    // The last number had been spoken before that, so get the delta between the last 2 times it was spoken and speak that as the new number.
+                    var delta = turnsItWasSpoken[^1] - turnsItWasSpoken[^2];
+                    memory.RecordNumber(delta);
+                }
+                else
+                {
+                    // This is the first time the last number had been spoken, so the new spoken number is 0.
+                    memory.RecordNumber(0);
+                }
+            }
+
+            return memory.LastNumberSpoken;
         }
 
-        protected override long? SolvePart2Impl(string input)
+        public class Memory
         {
-            return base.SolvePart2Impl(input);
+            private readonly Dictionary<int, List<int>> _numberToTurnsMap = new(); // Key is the Number; Value is the Turns that Number has been spoken.
+            private readonly List<int> _numbersSpoken = new();
+
+            public int LastNumberSpoken { get; private set; } = -1;
+
+            public int NextTurnNumber { get; private set; } = 1;
+
+            public IReadOnlyList<int> NumbersSpoken => _numbersSpoken;
+
+            public Memory(IEnumerable<int> seedNumbers) => seedNumbers.ForEach(RecordNumber);
+
+            public void RecordNumber(int number)
+            {
+                var turn = NextTurnNumber;
+
+                if (!_numberToTurnsMap.TryGetValue(number, out var listOfTurnsItWasSpoken))
+                {
+                    _numberToTurnsMap[number] = listOfTurnsItWasSpoken = new List<int>();
+                }
+
+                listOfTurnsItWasSpoken.Add(turn);
+                LastNumberSpoken = number;
+                _numbersSpoken.Add(number);
+                NextTurnNumber++;
+            }
+
+            public bool HasLastNumberBeenSpokenBefore(out IReadOnlyList<int> turnsItWasSpoken)
+            {
+                if (_numberToTurnsMap.TryGetValue(LastNumberSpoken, out var listOfTurnsItWasSpoken))
+                {
+                    turnsItWasSpoken = listOfTurnsItWasSpoken;
+                    return turnsItWasSpoken.Count > 1;
+                }
+
+                turnsItWasSpoken = Array.Empty<int>();
+                return false;
+            }
+
+            public static Memory SeedWith(string input) => new(input.Split(",").Select(int.Parse));
         }
     }
 }
