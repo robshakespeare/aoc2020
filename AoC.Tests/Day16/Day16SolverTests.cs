7aaf0dd3bee7098b07563aaf9b647d2818a81f6c
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2020-12-16 07:56:58 +0000

    Day 16, part 1 done

--- AoC.Tests/Day16/Day16SolverTests.cs
+++ AoC.Tests/Day16/Day16SolverTests.cs
@@ -1,12 +1,37 @@
+using System;
 using AoC.Day16;
 using FluentAssertions;
 using NUnit.Framework;
+using static AoC.Day16.Day16Solver;
 
 namespace AoC.Tests.Day16
 {
     public class Day16SolverTests
     {
         private readonly Day16Solver _sut = new();
+
+        private const string Example1 = @"class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12";
+
+        [Test]
+        public void Part1Example()
+        {
+            // ACT
+            var part1Result = _sut.SolvePart1(Example1);
+
+            // ASSERT
+            part1Result.Should().Be(71);
+        }
 
         [Test]
         public void Part1ReTest()
@@ -15,7 +40,7 @@ namespace AoC.Tests.Day16
             var part1Result = _sut.SolvePart1();
 
             // ASSERT
-            part1Result.Should().Be(null);
+            part1Result.Should().Be(23115);
         }
 
         [Test]
@@ -26,6 +51,60 @@ namespace AoC.Tests.Day16
 
             // ASSERT
             part2Result.Should().Be(null);
+        }
+
+        [Test]
+        public void TrainServiceNotesParsingTest()
+        {
+            // ACT
+            var result = TrainServiceNotes.Parse(Example1);
+
+            // ASSERT
+            result.Should().BeEquivalentTo(
+                new TrainServiceNotes(
+                    new[]
+                    {
+                        new FieldRule("class", new[] {new Range(1, 3), new Range(5, 7)}),
+                        new FieldRule("row", new[] {new Range(6, 11), new Range(33, 44)}),
+                        new FieldRule("seat", new[] {new Range(13, 40), new Range(45, 50)})
+                    },
+                    new Ticket(new[] {7, 1, 14}),
+                    new []
+                    {
+                        new Ticket(new[] {7, 3, 47}),
+                        new Ticket(new[] {40, 4, 50}),
+                        new Ticket(new[] {55, 2, 20}),
+                        new Ticket(new[] {38, 6, 12})
+                    }),
+                options => options
+                    .WithStrictOrdering()
+                    .ComparingByMembers<TrainServiceNotes>()
+                    .ComparingByMembers<FieldRule>()
+                    .ComparingByMembers<Ticket>());
+        }
+
+        [Test]
+        public void FieldRule_IsValidValue_Tests()
+        {
+            var sut = new FieldRule("test", new[]
+            {
+                new Range(1, 3),
+                new Range(5, 7)
+            });
+
+            // ASSERT
+            sut.IsValidValue(1).Should().BeTrue();
+            sut.IsValidValue(2).Should().BeTrue();
+            sut.IsValidValue(3).Should().BeTrue();
+
+            sut.IsValidValue(5).Should().BeTrue();
+            sut.IsValidValue(6).Should().BeTrue();
+            sut.IsValidValue(7).Should().BeTrue();
+
+            sut.IsValidValue(4).Should().BeFalse();
+            sut.IsValidValue(0).Should().BeFalse();
+            sut.IsValidValue(8).Should().BeFalse();
+            sut.IsValidValue(100).Should().BeFalse();
         }
     }
 }
--- AoC/Day16/Day16Solver.cs
+++ AoC/Day16/Day16Solver.cs
@@ -1,17 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using static System.Environment;
+
 namespace AoC.Day16
 {
     public class Day16Solver : SolverBase
     {
         public override string DayName => "";
 
-        protected override long? SolvePart1Impl(string input)
-        {
-            return base.SolvePart1Impl(input);
-        }
+        protected override long? SolvePart1Impl(string input) => TrainServiceNotes.Parse(input).GetInvalidTicketValues().Sum();
 
         protected override long? SolvePart2Impl(string input)
         {
             return base.SolvePart2Impl(input);
+        }
+
+        public record TrainServiceNotes(
+            IReadOnlyList<FieldRule> FieldRules,
+            Ticket YourTicket,
+            IReadOnlyList<Ticket> OtherTickets)
+        {
+            public static TrainServiceNotes Parse(string input)
+            {
+                var parts = input.NormalizeLineEndings().Split(NewLine + NewLine);
+
+                if (!parts[1].StartsWith("your ticket:") || !parts[2].StartsWith("nearby tickets:"))
+                {
+                    throw new InvalidOperationException("Ticket not valid: " + input);
+                }
+
+                return new TrainServiceNotes(
+                    FieldRule.ParseRules(parts[0]),
+                    Ticket.ParseTicket(parts[1].ReadLines().Skip(1).Single()),
+                    parts[2].ReadLines().Skip(1).Select(Ticket.ParseTicket).ToArray());
+            }
+
+            public IEnumerable<int> GetInvalidTicketValues()
+            {
+                return OtherTickets.SelectMany(ticket => ticket.GetInvalidValues(FieldRules));
+            }
+        }
+
+        public record FieldRule(
+            string FieldName,
+            IReadOnlyList<Range> Ranges)
+        {
+            private static readonly Regex ParseRuleRegex = new(@"(?<FieldName>\w+): ((?<Range>\d+-\d+)|( or ))+", RegexOptions.Compiled);
+
+            public static FieldRule[] ParseRules(string rules) => rules.ReadLines().Select(ParseRule).ToArray();
+
+            /// <summary>
+            /// The value is valid if it falls within any of the ranges.
+            /// </summary>
+            public bool IsValidValue(int value) => Ranges.Any(range => value >= range.Start.Value && value <= range.End.Value);
+
+            public static FieldRule ParseRule(string rule)
+            {
+                var match = ParseRuleRegex.Match(rule);
+                if (!match.Success)
+                {
+                    throw new InvalidOperationException("Rule not valid: " + rule);
+                }
+
+                return new FieldRule(
+                    match.Groups["FieldName"].Value,
+                    match.Groups["Range"].Captures.Select(c =>
+                    {
+                        var rangeParts = c.Value.Split("-");
+                        return new Range(int.Parse(rangeParts[0]), int.Parse(rangeParts[1]));
+                    }).ToArray());
+            }
+        }
+
+        public record Ticket(
+            IReadOnlyList<int> Values)
+        {
+            public static Ticket ParseTicket(string ticket) => new(ticket.Split(",").Select(int.Parse).ToArray());
+
+            /// <summary>
+            /// Each value is invalid if it does not match a single rule.
+            /// </summary>
+            public IEnumerable<int> GetInvalidValues(IEnumerable<FieldRule> rules) => Values.Where(value => !rules.Any(rule => rule.IsValidValue(value)));
         }
     }
 }
