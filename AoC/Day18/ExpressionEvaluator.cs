67f4f9f211f366847b14e85594750ce6c6661515
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2020-12-18 07:43:24 +0000

    Day 18, part 1, tidy

--- AoC/Day18/ExpressionEvaluator.cs
+++ AoC/Day18/ExpressionEvaluator.cs
@@ -1,8 +1,4 @@
-//using System.Linq.Expressions;
-
 using System;
-using System.Linq;
-using System.Runtime.InteropServices;
 using Sprache;
 
 namespace AoC.Day18
@@ -17,51 +13,11 @@ namespace AoC.Day18
 
         public static class ExpressionParser
         {
-            // Expr = Literal | Expr operator Expr | (Expr)
-
-            //public IExpression Parse(string expression)
-            //{
-            //    foreach (var chr in expression.Where(c => !char.IsWhiteSpace(c)))
-            //    {
-
-            //    }
-            //}
-
-            //private static readonly Parser<long> Literal2 =
-            //    from leading in Parse.WhiteSpace.Many()
-            //    from literal in Parse.Digit.AtLeastOnce().Text()
-            //    from trailing in Parse.WhiteSpace.Many()
-            //    select long.Parse(literal);
-
-            //private static readonly Parser<char> Operator =
-            //    from leading in Parse.WhiteSpace.Many()
-            //    from op in Parse.Chars('+', '*')
-            //    from trailing in Parse.WhiteSpace.Many()
-            //    select op;
-
             private static readonly Parser<IExpression> Literal =
                 from literal in Parse.Digit.AtLeastOnce().Token().Text()
                 select new LiteralExpr(long.Parse(literal));
 
             private static readonly Parser<char> Operator = Parse.Chars('+', '*').Token();
-            //from leading in Parse.WhiteSpace.Many()
-            //from op in Parse.Chars('+', '*').Token()
-            ////from trailing in Parse.WhiteSpace.Many()
-            //select op;
-
-            //static Parser<ExpressionType> Operator3(string op, ExpressionType opType)
-            //{
-            //    return Parse.String(op).Token().Return(opType);
-            //}
-
-            //private static readonly Parser<char> Expression =
-            //    from leading in Parse.WhiteSpace.Many()
-            //    from op in Parse.Chars('+', '*')
-            //    from trailing in Parse.WhiteSpace.Many()
-            //    select op;
-
-            //private static readonly Parser<IExpression> Expr =
-            //    Literal.Or(Expr);
 
             private static readonly Parser<IExpression> SubExpression =
                 from lp in Parse.Char('(').Token()
