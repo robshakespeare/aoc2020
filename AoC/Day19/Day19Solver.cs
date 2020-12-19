5f3ec0b4ee2463c5b9f5f82fdf1309ee9887fc51
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2020-12-19 07:34:44 +0000

    Day 19, part 2, hmmm

--- AoC/Day19/Day19Solver.cs
+++ AoC/Day19/Day19Solver.cs
@@ -23,7 +23,17 @@ namespace AoC.Day19
 
         protected override long? SolvePart2Impl(string input)
         {
-            return base.SolvePart2Impl(input);
+            input = input
+                .Replace("8: 42", "8: 42 | 42 8")
+                .Replace("11: 42 31", "11: 42 31 | 42 11 31");
+
+            var sections = input.NormalizeLineEndings().Split($"{Environment.NewLine}{Environment.NewLine}");
+
+            var rulesParser = ResolveFirstRuleToParser(sections[0]);
+
+            var receivedMessages = sections[1];
+
+            return receivedMessages.ReadLines().Count(receivedMessage => rulesParser.TryParse(receivedMessage).WasSuccessful);
         }
 
         private static readonly Regex ParseRawLine = new(
