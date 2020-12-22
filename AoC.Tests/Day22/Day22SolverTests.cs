ce92f52653b2c8d5bb5d661455fe99abf2c254ba
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2020-12-22 05:19:00 +0000

    Day 22, part 1 done

--- AoC.Tests/Day22/Day22SolverTests.cs
+++ AoC.Tests/Day22/Day22SolverTests.cs
@@ -9,13 +9,35 @@ namespace AoC.Tests.Day22
         private readonly Day22Solver _sut = new();
 
         [Test]
+        public void Part1Example()
+        {
+            // ACT
+            var part1Result = _sut.SolvePart1(@"Player 1:
+9
+2
+6
+3
+1
+
+Player 2:
+5
+8
+4
+7
+10");
+
+            // ASSERT
+            part1Result.Should().Be(306);
+        }
+
+        [Test]
         public void Part1ReTest()
         {
             // ACT
             var part1Result = _sut.SolvePart1();
 
             // ASSERT
-            part1Result.Should().Be(null);
+            part1Result.Should().Be(33421);
         }
 
         [Test]
--- AoC/Day22/Day22Solver.cs
+++ AoC/Day22/Day22Solver.cs
@@ -1,12 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace AoC.Day22
 {
     public class Day22Solver : SolverBase
     {
-        public override string DayName => "";
+        public override string DayName => "Crab Combat";
 
         protected override long? SolvePart1Impl(string input)
         {
-            return base.SolvePart1Impl(input);
+            IReadOnlyList<IReadOnlyList<int>> originalDecks = input
+                .NormalizeLineEndings()
+                .Split($"{Environment.NewLine}{Environment.NewLine}")
+                .Select(deck => deck.ReadLines().ToArray()[1..].Select(int.Parse).ToArray())
+                .ToArray();
+
+            var player1Deck = new Queue<int>(originalDecks[0]);
+            var player2Deck = new Queue<int>(originalDecks[1]);
+
+            if (player1Deck.Count != player2Deck.Count)
+            {
+                throw new InvalidOperationException("Player deck sizes do not match.");
+            }
+
+            var roundNumber = 0;
+            while (player1Deck.Any() && player2Deck.Any())
+            {
+                roundNumber++;
+                var player1Card = player1Deck.Dequeue();
+                var player2Card = player2Deck.Dequeue();
+
+                if (player1Card == player2Card)
+                {
+                    throw new InvalidOperationException($"Draw not supported! {new{ roundNumber, player1Card, player2Card }}");
+                }
+
+                var player1Won = player1Card > player2Card;
+
+                if (player1Won)
+                {
+                    player1Deck.Enqueue(player1Card);
+                    player1Deck.Enqueue(player2Card);
+                }
+                else
+                {
+                    player2Deck.Enqueue(player2Card);
+                    player2Deck.Enqueue(player1Card);
+                }
+            }
+
+            var winningDeck = player1Deck.Any() ? player1Deck : player2Deck;
+
+            return winningDeck.Reverse().Select((card, i) => (card, n: i + 1)).Aggregate(0L, (agg, cur) => agg + cur.card * cur.n);
         }
 
         protected override long? SolvePart2Impl(string input)
