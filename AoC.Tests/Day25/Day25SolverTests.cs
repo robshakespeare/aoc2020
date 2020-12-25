7ab7e98e5fd866c5f2f60f1967c00a69dd4a2c0f
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2020-12-25 06:46:54 +0000

    Day 25 done :)  Merry Christmas!

--- AoC.Tests/Day25/Day25SolverTests.cs
+++ AoC.Tests/Day25/Day25SolverTests.cs
@@ -8,6 +8,31 @@ namespace AoC.Tests.Day25
     {
         private readonly Day25Solver _sut = new();
 
+        [TestCase(5764801, 8)]
+        [TestCase(17807724, 11)]
+        public void DetermineLoopSize_Tests(int publicKey, int expectedLoopSize)
+        {
+            Day25Solver.DetermineLoopSize(publicKey).Should().Be(expectedLoopSize);
+        }
+
+        [TestCase(17807724, 8, 14897079)]
+        [TestCase(5764801, 11, 14897079)]
+        public void TransformSubjectNumber_Tests(int subjectNumber, int loopSize, int expectedEncryptionKey)
+        {
+            Day25Solver.TransformSubjectNumber(subjectNumber, loopSize).Should().Be(expectedEncryptionKey);
+        }
+
+        [Test]
+        public void Part1Example()
+        {
+            // ACT
+            var part1Result = _sut.SolvePart1(@"5764801
+17807724");
+
+            // ASSERT
+            part1Result.Should().Be(14897079);
+        }
+
         [Test]
         public void Part1ReTest()
         {
@@ -15,7 +40,7 @@ namespace AoC.Tests.Day25
             var part1Result = _sut.SolvePart1();
 
             // ASSERT
-            part1Result.Should().Be(null);
+            part1Result.Should().Be(1478097);
         }
 
         [Test]
@@ -25,7 +50,7 @@ namespace AoC.Tests.Day25
             var part2Result = _sut.SolvePart2();
 
             // ASSERT
-            part2Result.Should().Be(null);
+            part2Result.Should().StartWith("Day 25, part 2, was free :)");
         }
     }
 }
--- AoC/Day25/Day25Solver.cs
+++ AoC/Day25/Day25Solver.cs
@@ -11,20 +11,72 @@
 // .                                                                                                     .
 // .:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:*~*:._.:*~*:.
 
+using System;
+
 namespace AoC.Day25
 {
-    public class Day25Solver : SolverBase
+    public class Day25Solver : SolverBase<long, string>
     {
-        public override string DayName => "";
+        public override string DayName => "Combo Breaker";
 
-        protected override long? SolvePart1Impl(string input)
+        /// <summary>
+        /// What encryption key is the handshake trying to establish?
+        /// </summary>
+        protected override long SolvePart1Impl(string input)
         {
-            return base.SolvePart1Impl(input);
+            var publicKeys = input.NormalizeLineEndings().Split(Environment.NewLine);
+            var cardPublicKey = int.Parse(publicKeys[0]);
+            var doorPublicKey = int.Parse(publicKeys[1]);
+
+            var cardLoopSize = DetermineLoopSize(cardPublicKey);
+
+            return TransformSubjectNumber(doorPublicKey, cardLoopSize);
         }
 
-        protected override long? SolvePart2Impl(string input)
+        public static int DetermineLoopSize(int publicKey)
         {
-            return base.SolvePart2Impl(input);
+            const int subjectNumber = 7;
+            var loopSize = 0;
+            var value = 1;
+
+            while (value != publicKey)
+            {
+                loopSize++;
+                value *= subjectNumber;
+                value %= 20201227;
+            }
+
+            return loopSize;
         }
+
+        public static long TransformSubjectNumber(int subjectNumber, int loopSize)
+        {
+            long value = 1;
+
+            for (var i = 0; i < loopSize; i++)
+            {
+                value *= subjectNumber;
+                value %= 20201227;
+            }
+
+            return value;
+        }
+
+        private const string MerryChristmas = @"
+.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:*~*:._.:*~*:.
+.                                                                                                     .
+.       *         __  __                         _____ _          _     _                       _     .
+.      /.\       |  \/  |                       / ____| |        (_)   | |                     | |    .
+.     /..'\      | \  / | ___ _ __ _ __ _   _  | |    | |__  _ __ _ ___| |_ _ __ ___   __ _ ___| |    .
+.     /'.'\      | |\/| |/ _ \ '__| '__| | | | | |    | '_ \| '__| / __| __| '_ ` _ \ / _` / __| |    .
+.    /.''.'\     | |  | |  __/ |  | |  | |_| | | |____| | | | |  | \__ \ |_| | | | | | (_| \__ \_|    .
+.    /.'.'.\     |_|  |_|\___|_|  |_|   \__, |  \_____|_| |_|_|  |_|___/\__|_| |_| |_|\__,_|___(_)    .
+.   /'.''.'.\                            __/ |                                                        .
+.   ^^^[_]^^^                           |___/                                                         .
+.                                                                                                     .
+.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:*~*:._.:*~*:.
+";
+
+        protected override string SolvePart2Impl(string input) => $"Day 25, part 2, was free :){Environment.NewLine}{MerryChristmas}";
     }
 }
